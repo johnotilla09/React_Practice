138bf6e4bf53537381f1ed0670b4c82b9073f73f
John Otilla <johncanila.otilla@bicol-u.edu.ph>
2023-04-10 23:21:09 +0800

    [200~

--- expense_tracker/src/components/Expenses/Expenses.js
+++ expense_tracker/src/components/Expenses/Expenses.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-import ExpenseItem from "./ExpenseItem";
+import ExpenseList from "./ExpenseList";
 import Card from "../UI/Card";
 import './Expenses.css';
 import ExpenseFilter from "./ExpensesFilter";
@@ -19,48 +19,13 @@ function Expenses(props) {
     return date.getFullYear().toString() === filterYear;
   });
 
-  let expensesContent = <p>No Expenses Found</p>;
-
-  if (filteredExpenses.length > 0) {
-    expensesContent = filteredExpenses.map((expense) => (
-      <ExpenseItem
-        key={expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      />
-    ));
-  }
-  // Outputting Conditional Contents
-
   return (
     <Card className="expenses">
       <ExpenseFilter
         selected={filterYear}
         onChangeFilter={filterChangeHandler}
       />
-      {/* Outputting Conditional Content */}
-      {expensesContent}
-      {/* <ExpenseItem
-        title={props.items[0].title}
-        amount={props.items[0].amount}
-        date={props.items[0].date}
-      />
-      <ExpenseItem
-        title={props.items[1].title}
-        amount={props.items[1].amount}
-        date={props.items[1].date}
-      />
-      <ExpenseItem
-        title={props.items[2].title}
-        amount={props.items[2].amount}
-        date={props.items[2].date}
-      />
-      <ExpenseItem
-        title={props.items[3].title}
-        amount={props.items[3].amount}
-        date={props.items[3].date}
-      /> */}
+      <ExpenseList items={filteredExpenses} />
     </Card>
   );
 }
