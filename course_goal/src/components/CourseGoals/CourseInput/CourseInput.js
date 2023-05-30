5f38ce3e297a4043395829b46fccc33d10d93cff
JohnOtilla09 <otillajohn09@gmail.com>
2023-05-30 15:35:12 +0800

    008 Dynamic Styles with CSS Modules

--- course_goal/src/components/CourseGoals/CourseInput/CourseInput.js
+++ course_goal/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -2,45 +2,45 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 
 import Button from '../../UI/Button/Button';
-import './CourseInput.css';
+import styles from './CourseInput.module.css';
 
 // Other wau to style a compo
-const FormControl = styled.div`
-    margin: 0.5rem 0;
+// const FormControl = styled.div`
+//     margin: 0.5rem 0;
 
-  & label {
-    font-weight: bold;
-    display: block;
-    margin-bottom: 0.5rem;
-    color: ${props => props.invalid ? '#FF0000' : '#000000'};
-  }
+//   & label {
+//     font-weight: bold;
+//     display: block;
+//     margin-bottom: 0.5rem;
+//     color: ${props => props.invalid ? '#FF0000' : '#000000'};
+//   }
 
-  & input {
-    display: block;
-    width: 100%;
-    border: 1px solid ${props => (props.invalid ? '#FF0000' : '#ccc')};
-    background: ${props => props.invalid ? '#ffd7d7' : 'transparent'};
-    font: inherit;
-    line-height: 1.5rem;
-    padding: 0 0.25rem;
-  }
+//   & input {
+//     display: block;
+//     width: 100%;
+//     border: 1px solid ${props => (props.invalid ? '#FF0000' : '#ccc')};
+//     background: ${props => props.invalid ? '#ffd7d7' : 'transparent'};
+//     font: inherit;
+//     line-height: 1.5rem;
+//     padding: 0 0.25rem;
+//   }
 
-  & input:focus {
-    outline: none;
-    background: #fad0ec;
-    border-color: #8b005d;
-  }
+//   & input:focus {
+//     outline: none;
+//     background: #fad0ec;
+//     border-color: #8b005d;
+//   }
 
-  &.invalid input {
-    border-color: #FF0000;
-    background: #ffd7d7;
-  }
+//   &.invalid input {
+//     border-color: #FF0000;
+//     background: #ffd7d7;
+//   }
 
-  &.invalid label {
-    color: #FF0000;
-  }
+//   &.invalid label {
+//     color: #FF0000;
+//   }
 
-`;
+// `;
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
@@ -64,13 +64,13 @@ const CourseInput = props => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <FormControl invalid={!isValid}>
+      <div className={`${styles['form-control']} ${!isValid && styles.invalid}`}>
         <label>Course Goal</label>
         <input
           type="text"
           onChange={goalInputChangeHandler}
         />
-      </FormControl>
+      </div>
       <Button type="submit">Add Goal</Button>
     </form>
   );
