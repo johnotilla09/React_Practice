079745384f8bb296c39eadefc077d5410c5b4e29
JohnOtilla09 <otillajohn09@gmail.com>
2023-06-02 21:54:34 +0800

    010 Managing the Error State

--- complete-guide-project/src/components/UI/ErrorModal/ErrorModal.js
+++ complete-guide-project/src/components/UI/ErrorModal/ErrorModal.js
@@ -7,17 +7,18 @@ import styles from './ErrorModal.module.css';
 const ErrorModal = (props) => {
     return (
         <div>
-            <div className={styles.backdrop} />
-            <div className={`${styles['modal']} ${styles['card']}`}>
-                <header className={styles.header}>
-                    <h2>{props.title}</h2>
-                </header>
-                <div className={styles.content}>
-                    <p>{props.message}</p>
+            <div className={styles.backdrop} onClick={props.onConfirm}>
+                <div className={`${styles['modal']} ${styles['card']}`}>
+                    <header className={styles.header}>
+                        <h2>{props.title}</h2>
+                    </header>
+                    <div className={styles.content}>
+                        <p>{props.message}</p>
+                    </div>
+                    <footer className={styles.actions}>
+                        <Button className={styles.button} onClick={props.onConfirm}>Okay</Button>
+                    </footer>
                 </div>
-                <footer className={styles.actions}>
-                    <Button className={styles.button}>Okay</Button>
-                </footer>
             </div>
         </div>
     );
--- complete-guide-project/src/components/Users/AddUser.js
+++ complete-guide-project/src/components/Users/AddUser.js
@@ -12,15 +12,24 @@ const AddUser = (props) => {
     const [enteredLastname, setEnteredLastname] = useState('');
     const [enteredAddress, setEnteredAddress] = useState('');
     const [enteredAge, setEnteredAge] = useState('');
+    const [error, setError] = useState();
 
     const addUserHandler = (event) => {
         event.preventDefault();
 
         if (enteredFirstname.trim().length === 0 || enteredLastname.trim().length === 0 || enteredAddress.trim().length === 0 || enteredAge.trim().length === 0) {
+            setError({
+                title: 'Invalid Input',
+                message: 'Please enter a valid name, age and address (non-empty values).'
+            });
             return;
         }
 
         if (enteredAge < 1) {
+            setError({
+                title: 'Invalid Age',
+                message: 'Please enter a valid age greater zero (0).'
+            });
             return;
         }
 
@@ -48,36 +57,70 @@ const AddUser = (props) => {
         setEnteredAge(event.target.value);
     };
     
+    const errorHandler = (props) => {
+        setError(null);
+    }
+    
     return (
-
-        <div>
-            <ErrorModal title="An Error Occured" message="Something went wrong!" />
-            <Card>
-                <h2>Add User</h2>
-                <form onSubmit={addUserHandler}>
-                    <div className={`${styles['names']}`}>
-                        <div className={`${styles['form-group']} ${styles['firstname']}`}>
-                            <input type="text" value={enteredFirstname} onChange={firstnameChangeHandler} />
-                        </div>
-                        <div className={`${styles['form-group']} ${styles['lastname']}`}>
-                            <input type="text" value={enteredLastname} onChange={lastnameChangeHandler} />
-                        </div>
-                    </div>
-                    <div className={`${styles['address']}`}>
-                        <div className={`${styles['form-group']} ${styles['home-address']}`}>
-                            <input type="text" value={enteredAddress} onChange={addressChangeHandler} />
-                        </div>
-                    </div>
-                    <div className={`${styles['age']}`}>
-                        <div className={`${styles['form-group']} ${styles['user-age']}`}>
-                            <input type="text" value={enteredAge} onChange={ageChangeHandler} />
-                        </div>
-                    </div>
-                    <Button type="submit">Add User</Button>
-                </form>
-            </Card>
-        </div>
-
+      <div>
+        {error && (
+          <ErrorModal
+            title={error.title}
+            message={error.message}
+            onConfirm={errorHandler}
+          />
+        )}
+        <Card>
+          <h2>Add User</h2>
+          <form onSubmit={addUserHandler}>
+            <div className={`${styles["names"]}`}>
+              <div className={`${styles["form-group"]}`}>
+                <label htmlFor="firstname">First Name</label>
+                <input
+                  id="firstname"
+                  type="text"
+                  value={enteredFirstname}
+                  onChange={firstnameChangeHandler}
+                />
+              </div>
+              <div className={`${styles["form-group"]}`}>
+              <label htmlFor="lastname">Last Name</label>
+                <input
+                  id="lastname"
+                  type="text"
+                  value={enteredLastname}
+                  onChange={lastnameChangeHandler}
+                />
+              </div>
+            </div>
+            <div className={`${styles["address"]}`}>
+              <div
+                className={`${styles["form-group"]}`}
+              >
+                <label htmlFor="address">Address</label>
+                <input
+                  id="address"
+                  type="text"
+                  value={enteredAddress}
+                  onChange={addressChangeHandler}
+                />
+              </div>
+            </div>
+            <div className={`${styles["age"]}`}>
+              <div className={`${styles["form-group"]}`}>
+              <label htmlFor="age">Age</label>
+                <input
+                  id="age"
+                  type="text"
+                  value={enteredAge}
+                  onChange={ageChangeHandler}
+                />
+              </div>
+            </div>
+            <Button type="submit">Add User</Button>
+          </form>
+        </Card>
+      </div>
     );
 };
 
