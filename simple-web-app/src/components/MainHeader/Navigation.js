8f8d24491b3f8288a2dcb7305f37e95089e9a721
JohnOtilla09 <otillajohn09@gmail.com>
2023-06-06 19:16:03 +0800

    Module 10: 015 Tapping Into Context with the useContext Hook

--- simple-web-app/src/components/MainHeader/Navigation.js
+++ simple-web-app/src/components/MainHeader/Navigation.js
@@ -1,36 +1,34 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import classes from './Navigation.module.css';
 import AuthContext from '../store/auth-context';
 
 const Navigation = (props) => {
+
+  // useContext Hook
+  const ctx = useContext(AuthContext);
+
   return (
-     // Using React Context
-    <AuthContext.Consumer>
-      {(ctx) => {
-        return (
-          <nav className={classes.nav}>
-            <ul>
-              {ctx.isLoggedIn && (
-                <li>
-                  <a href="/">Users</a>
-                </li>
-              )}
-              {ctx.isLoggedIn && (
-                <li>
-                  <a href="/">Admin</a>
-                </li>
-              )}
-              {ctx.isLoggedIn && (
-                <li>
-                  <button onClick={props.onLogout}>Logout</button>
-                </li>
-              )}
-            </ul>
-          </nav>
-        )
-      }}
-    </AuthContext.Consumer>
+    // Using React Context
+    <nav className={classes.nav}>
+      <ul>
+        {ctx.isLoggedIn && (
+          <li>
+            <a href="/">Users</a>
+          </li>
+        )}
+        {ctx.isLoggedIn && (
+          <li>
+            <a href="/">Admin</a>
+          </li>
+        )}
+        {ctx.isLoggedIn && (
+          <li>
+            <button onClick={props.onLogout}>Logout</button>
+          </li>
+        )}
+      </ul>
+    </nav>
     // Using React Context
   );
 };
