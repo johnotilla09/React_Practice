6dcd223361aa42cfbb646c93550bdead4507f55f
JohnOtilla09 <otillajohn09@gmail.com>
2023-06-06 23:19:34 +0800

    Module 10: 021 Diving into _Forward Refs_

--- simple-web-app/src/components/Login/Login.js
+++ simple-web-app/src/components/Login/Login.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useReducer, useContext } from 'react';
+import React, { useState, useEffect, useReducer, useContext, useRef } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
@@ -57,6 +57,8 @@ const Login = (props) => {
   // This is how to use the useReducer inside the component
 
   const authCtx = useContext(AuthContext);
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
 
   const {isValid: emailIsValid} = emailState;
   const{isValid: passwordIsValid} = passwordState;
@@ -103,13 +105,20 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    authCtx.onLogin(emailState.value, passwordState.value);
+    if (formIsValid) {
+      authCtx.onLogin(emailState.value, passwordState.value);
+    } else if (!emailIsValid) {
+      emailInputRef.current.focus();
+    } else {
+      passwordInputRef.current.focus();
+    }
   };
 
   return (
     <Card className={classes.login}>
       <form onSubmit={submitHandler}>
         <Input
+          ref={emailInputRef}
           id="email"
           label="E-Mail"
           type="email"
@@ -119,6 +128,7 @@ const Login = (props) => {
           onBlur={validateEmailHandler}
         />
         <Input
+          ref={passwordInputRef}
           id="password"
           label="Password"
           type="password"
@@ -142,7 +152,7 @@ const Login = (props) => {
           />
         </div> */}
         <div className={classes.actions}>
-          <Button type="submit" className={classes.btn} disabled={!formIsValid}>
+          <Button type="submit" className={classes.btn} >
             Login
           </Button>
         </div>
--- simple-web-app/src/components/UI/Input/Input.js
+++ simple-web-app/src/components/UI/Input/Input.js
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { useRef, useImperativeHandle } from 'react';
 
 import classes from './Input.module.css';
 
-const Input = (props) => {
+const Input = React.forwardRef((props, ref) => {
+    const inputRef = useRef();
+
+    const activate = () => {
+        inputRef.current.focus();
+    };
+
+    useImperativeHandle(ref, () => {
+        return {
+            focus: activate
+        };
+    });
+
+    // useEffect(() => {
+    //     inputRef.current.focus();
+    // }, []);
+
     // Refactoring an input component
     return (
       <div
@@ -12,6 +28,7 @@ const Input = (props) => {
       >
         <label htmlFor={props.id}>{props.label}</label>
         <input
+          ref={inputRef}
           type={props.type}
           id={props.id}
           value={props.value}
@@ -21,6 +38,6 @@ const Input = (props) => {
       </div>
     );
     // Refactoring an input component
-};
+});
 
 export default Input;
