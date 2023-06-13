72b8f97b802b11b34f04d27175fc594c85d55d60
JohnOtilla09 <otillajohn09@gmail.com>
2023-06-13 08:50:53 +0800

    Module 13: 003 Adding a First Class-based Component

--- user-toggle/src/components/User.js
+++ user-toggle/src/components/User.js
@@ -1,7 +1,17 @@
+import { Component } from 'react';
+
 import classes from './User.module.css';
 
-const User = (props) => {
-  return <li className={classes.user}>{props.name}</li>;
-};
+// Class Based Components
+class User extends Component {
+  render() {
+    return <li className={classes.user}>{this.props.name}</li>;
+  }
+}
+
+// Functional Components
+// const User = (props) => {
+//   return <li className={classes.user}>{props.name}</li>;
+// };
 
 export default User;
