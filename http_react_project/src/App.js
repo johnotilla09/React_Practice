486e773af352c5b29fcac29dc15a2ddd051fb2bb
JohnOtilla09 <otillajohn09@gmail.com>
2023-06-15 00:24:45 +0800

    Module 14: 006 Using async _ await

--- http_react_project/src/App.js
+++ http_react_project/src/App.js
@@ -6,26 +6,45 @@ import './App.css';
 function App() {
   const [movies, setMovies] = useState([]);
 
-  // HTTP request
-  const fetchMovieHandler = () => {
-    // requesting and getting response to the sapi
-    fetch('https://swapi.dev/api/films/').then(response => {
-      return response.json();
-    }).then(data => {
-      // Change the name of the variables
-      const transformedMovies = data.results.map(movieData => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date
-        };
-      })
-      setMovies(transformedMovies);
+  async function fetchMovieHandler () {
+    // Request to the API
+    const response = await fetch('https://swapi.dev/api/films/');
+    // Get response through json format
+    const data = await response.json();
+    transform the data key
+    const transformedMovies = data.results.map((movieData) => {
+      return {
+        id: movieData.episode_id,
+        title: movieData.title,
+        openingText: movieData.opening_crawl,
+        releaseDate: movieData.release_date
+      };
     });
-  };
 
-  console.log(movies.title);
+    // set the data to the movie state
+    setMovies(transformedMovies);
+  }
+
+  // The other way of requesting and getting a response to the request
+  // HTTP request
+  // const fetchMovieHandler = () => {
+  //   // requesting and getting response to the sapi
+  //   fetch('https://swapi.dev/api/films/').then(response => {
+  //     return response.json();
+  //   }).then(data => {
+  //     // Change the name of the variables
+  //     const transformedMovies = data.results.map(movieData => {
+  //       return {
+  //         id: movieData.episode_id,
+  //         title: movieData.title,
+  //         openingText: movieData.opening_crawl,
+  //         releaseDate: movieData.release_date
+  //       };
+  //     })
+  //     setMovies(transformedMovies);
+  //   });
+
+  // console.log(movies.title);
 
   return (
     <React.Fragment>
