89f94db234ed725de9fc3fbcf49ce4bfbcfae270
JohnOtilla09 <otillajohn09@gmail.com>
2023-06-29 19:02:51 +0800

    Module 17: 003 Fetching Meals via Http

--- techtrove/src/components/Products/AvailableProducts.js
+++ techtrove/src/components/Products/AvailableProducts.js
@@ -1,39 +1,36 @@
-import React from "react";
+import React,{ useEffect, useState } from "react";
 
 import Card from "../UI/Card";
 import ProductsItem from "./ProductsItem/ProductsItem";
 
 import styles from './AvailableProducts.module.css';
 
-const DUMMY_PRODUCTS = [
-    {
-    id: 'p1',
-    name: 'Graphics Card',
-    description: 'Powerful graphics processing unit',
-    price: 499.99,
-    },
-    {
-    id: 'p2',
-    name: 'CPU',
-    description: 'High-performance central processing unit',
-    price: 349.99,
-    },
-    {
-    id: 'p3',
-    name: 'RAM Kit',
-    description: 'Fast and reliable memory modules',
-    price: 149.99,
-    },
-    {
-    id: 'p4',
-    name: 'SSD',
-    description: 'High-speed solid-state drive',
-    price: 129.99,
-    },
-];
-
 const AvailableProducts = (props) => {
-    const productsList = DUMMY_PRODUCTS.map((product) => (
+  const [ products, setProducts ] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const response = await fetch('https://react-html-db32f-default-rtdb.asia-southeast1.firebasedatabase.app/meals.json').then();
+      const responseData = await response.json();
+
+      const loadProducts = [];
+
+      for (const key in responseData) {
+        loadProducts.push({
+          id: key,
+          name: responseData[key].name,
+          description: responseData[key].description,
+          price: responseData[key].price
+        })
+      }
+
+      setProducts(loadProducts)
+    };
+
+    fetchProducts();
+  }, []);
+
+    const productsList = products.map((product) => (
       <ProductsItem
         key={product.id}
         product={product}
