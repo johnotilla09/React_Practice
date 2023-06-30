b90b1a6d7df94fdb44aa4a248c05ff3c38bed13a
JohnOtilla09 <otillajohn09@gmail.com>
2023-06-30 10:54:46 +0800

    Module 17: 009 Submitting & Sending Cart Data

--- techtrove/src/components/Cart/CheckOut.js
+++ techtrove/src/components/Cart/CheckOut.js
@@ -1,8 +1,18 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 
-import classes from './CheckOut.module.css';
+import styles from './CheckOut.module.css';
+
+const isEmpty = value => value.trim() === '';
+const isNotFiveChars = value => value.trim().length !== 5;
 
 const Checkout = (props) => {
+    const [ formInputsValidity, setFormInputsValidity ] = useState({
+        name: true,
+        zipCode: true,
+        municipality: true,
+        address: true
+    });
+
   const nameInputRef = useRef();
   const zipCodeInputRed = useRef();
   const municipalityInputref = useRef();
@@ -15,31 +25,73 @@ const Checkout = (props) => {
     const enteredZipCode = zipCodeInputRed.current.value;
     const enteredMunicipality = municipalityInputref.current.value;
     const enteredAddress = addressInputRef.current.value;
+
+    const enteredNameIsValid = !isEmpty(enteredName);
+    const enteredZipCodeIsValid = !isNotFiveChars(enteredZipCode);
+    const enteredMunicipalityIsValid = !isEmpty(enteredMunicipality);
+    const enteredAddressIsValid = !isEmpty(enteredAddress);
+
+    setFormInputsValidity({
+        name: enteredNameIsValid,
+        zipCode: enteredZipCodeIsValid,
+        municipality: enteredMunicipalityIsValid,
+        address: enteredAddressIsValid
+    });
+
+    const formIsValid =
+      enteredNameIsValid &&
+      enteredZipCodeIsValid &&
+      enteredMunicipalityIsValid &&
+      enteredAddressIsValid;
+    
+      if (!formIsValid) {
+        return;
+      }
+
+    console.log(formInputsValidity.name);
+
   };
 
+  const nameControlStyles = `${styles.control} ${
+    !formInputsValidity.name && styles.invalid
+  }`;
+  const zipCodeControlStyles = `${styles.control} ${
+    !formInputsValidity.zipCode && styles.invalid
+  }`;
+  const municipalityControlStyles = `${styles.control} ${
+    !formInputsValidity.municipality && styles.invalid
+  }`;
+  const addressControlStyles = `${styles.control} ${
+    !formInputsValidity.address && styles.invalid
+  }`;
+
   return (
-    <form className={classes.form} onSubmit={confirmHandler}>
-      <div className={classes.control}>
+    <form className={styles.form} onSubmit={confirmHandler}>
+      <div className={nameControlStyles}>
         <label htmlFor='name'>Your Name</label>
         <input type='text' id='name' ref={nameInputRef} />
+        { !formInputsValidity.name && <p>Please enter a valid name.</p> }
       </div>
-      <div className={classes.control}>
+      <div className={zipCodeControlStyles}>
         <label htmlFor='zip-code'>Zip Code</label>
         <input type='text' id='zip-code' ref={zipCodeInputRed} />
+        { !formInputsValidity.zipCode && <p>Please enter a valid zip code.</p> }
       </div>
-      <div className={classes.control}>
+      <div className={municipalityControlStyles}>
         <label htmlFor='municipality'>Municipality</label>
         <input type='text' id='municipality' ref={municipalityInputref} />
+        { !formInputsValidity.municipality && <p>Please enter a valid municipality.</p> }
       </div>
-      <div className={classes.control}>
+      <div className={addressControlStyles}>
         <label htmlFor='address'>Address</label>
-        <input type='text' id='address' />
+        <input type='text' id='address' ref={addressInputRef} />
+        { !formInputsValidity.address && <p>Please enter a valid address.</p> }
       </div>
-      <div className={classes.actions}>
+      <div className={styles.actions}>
         <button type='button' onClick={props.onCancel}>
           Cancel
         </button>
-        <button className={classes.submit}>Confirm</button>
+        <button className={styles.submit}>Confirm</button>
       </div>
     </form>
   );
