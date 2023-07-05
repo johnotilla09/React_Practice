257dd7c4bb1598f74bfd36ddbfe54775d953da1a
JohnOtilla09 <otillajohn09@gmail.com>
2023-07-05 18:01:15 +0800

    module 18: project prepareation and 008 Creating a Redux Store for React

---
+++ m18-using-redux-in-react/src/store/index.js
@@ -0,0 +1,60 @@
+// This is in react
+import { legacy_createStore } from "redux";
+
+// reducer function
+const counterReducer = (state = { counter: 0 }, action) => {
+    if (action.type === 'increment') {
+      return {
+        counter: state.counter + 1,
+      };
+    }
+        
+    if (action.type === "decrement") {
+      return {
+        counter: state.counter - 1,
+      };
+    }
+
+    return state;
+}
+
+const store = legacy_createStore(counterReducer);
+
+export default store;
+
+// This work in javascript
+// // import redux
+// const redux = require('redux');
+
+// // reducer function
+// const counterReducer = (state = { counter: 0 }, action) => {
+//     if (action.type === 'increment') {
+//         return {
+//             counter: state.counter + 1
+//         };
+//     }
+
+//     if (action.type === 'decrement') {
+//         return {
+//             counter: state.counter - 1
+//         };
+//     }
+
+//     return state;
+// };
+
+// // creating a store
+// const store = redux.legacy_createStore(counterReducer);
+
+// //  the subscriber
+// const subscriber = () => {
+//     const latestState = store.getState();
+//     console.log(latestState);
+// };
+
+// // calling the subcriber
+// store.subscribe(subscriber);
+
+// // dispatch
+// store.dispatch({ type: 'increment' });
+// store.dispatch({ type: 'decrement' });
