efdc099344cb2cff74feadc28c8ac1f7c5349a7a
JohnOtilla09 <otillajohn09@gmail.com>
2023-07-05 19:04:50 +0800

    Module 18: 010 Using Redux Data in React Components

--- m18-using-redux-in-react/src/components/Counter.js
+++ m18-using-redux-in-react/src/components/Counter.js
@@ -1,12 +1,20 @@
+// import a hook
+// useSelector hook - automatically select a part state manage by the store
+// connect function a wrapper around class compoenent to connect the class component to the stoore
+import { useSelector } from 'react-redux';
+
 import classes from './Counter.module.css';
 
 const Counter = () => {
+
+  const counter = useSelector(state => state.counter);
+
   const toggleCounterHandler = () => {};
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{ counter }</div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
