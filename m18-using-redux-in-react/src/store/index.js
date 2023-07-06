795888da9b54af9dc082c5ab2cea435298cd4ade
JohnOtilla09 <otillajohn09@gmail.com>
2023-07-06 20:56:35 +0800

    Module 18: 017 Adding State Slices

--- m18-using-redux-in-react/src/store/index.js
+++ m18-using-redux-in-react/src/store/index.js
@@ -1,11 +1,31 @@
 // import redux in react
 import { legacy_createStore } from "redux";
+import { createSlice } from "@reduxjs/toolkit";
 
 // 014 Working with Multiple State Properties
 const initialState = {
     counter: 0,
     showCounter: true
 }
+
+createSlice({
+  name: 'counter',
+  initialState,
+  reducers: {
+    increment(state) {
+      state.counter++;
+    },
+    increase(state, action) {
+      state.counter + action.amount;
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    toggleCounter(state, action) {
+      state.showCounter = !state.showCounter;
+    }
+  }
+});
 
 // reducer function
 const counterReducer = (state = initialState, action) => {
@@ -44,6 +64,53 @@ const counterReducer = (state = initialState, action) => {
 const store = legacy_createStore(counterReducer);
 
 export default store;
+
+// // import redux in react
+// import { legacy_createStore } from "redux";
+
+// // 014 Working with Multiple State Properties
+// const initialState = {
+//     counter: 0,
+//     showCounter: true
+// }
+
+// // reducer function
+// const counterReducer = (state = initialState, action) => {
+//     if (action.type === 'increment') {
+//       return {
+//         counter: state.counter + 1,
+//         showCounter: state.showCounter
+//       };
+//     }
+
+//     if (action.type === "increase") {
+//         return {
+//           counter: state.counter + action.amount,
+//           showCounter: state.showCounter
+//         };
+//       }
+        
+//     if (action.type === "decrement") {
+//       return {
+//         counter: state.counter - 1,
+//         showCounter: state.showCounter
+//       };
+//     }
+
+//     if (action.type === 'toggle') {
+//         return {
+//             showCounter: !state.showCounter,
+//             counter: state.counter
+//         };
+//     }
+
+//     return state;
+// }
+
+// // creating a store
+// const store = legacy_createStore(counterReducer);
+
+// export default store;
 
 // This work in javascript
 // // import redux
