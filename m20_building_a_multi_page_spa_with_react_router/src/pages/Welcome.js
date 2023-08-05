81463b283f8664f013482be97cc8b6c1ddbd0c3a
JohnOtilla09 <otillajohn09@gmail.com>
2023-08-05 18:58:34 +0800

    m20 | 010 Working with Nested Routes.mp4

--- m20_building_a_multi_page_spa_with_react_router/src/pages/Welcome.js
+++ m20_building_a_multi_page_spa_with_react_router/src/pages/Welcome.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { Route } from 'react-router-dom';
 
 const Welcome = () => {
-    return <h1>The Welcome Page.</h1>;
+    return (
+      <Fragment>
+        <h1>The Welcome Page.</h1>
+        <Route path="/welcome/new-user">
+          <p>Welcome, new user!</p>
+        </Route>
+      </Fragment>
+    );
 };
 
 export default Welcome;
