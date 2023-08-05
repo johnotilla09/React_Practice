49bc7e24611d2e4f3054e3b2dd77f36f0afa1e34
JohnOtilla09 <otillajohn09@gmail.com>
2023-08-05 09:57:08 +0800

    m20 | 005 Working with Links.mp4

--- m20_building_a_multi_page_spa_with_react_router/src/App.js
+++ m20_building_a_multi_page_spa_with_react_router/src/App.js
@@ -5,17 +5,21 @@ import { Route } from 'react-router-dom';
  // components
 import Welcome from './pages/Welcome';
 import Products from './pages/Products';
+import MainHeader from './components/MainHeader';
 
 const App = () => {
   return (
     <div>
-      <Route path="/welcome">
-        <Welcome />
-      </Route>
+      <MainHeader/>
+      <main>
+        <Route path="/welcome">
+          <Welcome />
+        </Route>
 
-      <Route path="/products">
-        <Products />
-      </Route>
+        <Route path="/products">
+          <Products />
+        </Route>
+      </main>
     </div>
   );
 }
