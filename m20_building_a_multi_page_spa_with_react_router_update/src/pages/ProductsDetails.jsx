228acdb2cb502b12bf3dab00476fcbd885b12648
JohnOtilla09 <johncanila.otilla@bicol-u.edu.ph>
2024-01-16 21:35:45 +0800

    React Router | 008 Extracting Route Params

--- m20_building_a_multi_page_spa_with_react_router_update/src/pages/ProductsDetails.jsx
+++ m20_building_a_multi_page_spa_with_react_router_update/src/pages/ProductsDetails.jsx
@@ -1,7 +1,12 @@
+import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+
 const ProductsDetails = () => {
+    const params = useParams();
+
     return (
         <section>
             <h1>Products Details</h1>
+            <p>{params.productId}</p>
         </section>
     );
 };
