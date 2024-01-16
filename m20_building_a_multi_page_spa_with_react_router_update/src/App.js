1767976636a5eb90655847491fc26bc6f7422b88
JohnOtilla09 <johncanila.otilla@bicol-u.edu.ph>
2024-01-16 18:12:43 +0800

    React Router | 005 Working with Links.mp4

--- m20_building_a_multi_page_spa_with_react_router_update/src/App.js
+++ m20_building_a_multi_page_spa_with_react_router_update/src/App.js
@@ -1,17 +1,22 @@
 import { Route, Router } from "react-router-dom/cjs/react-router-dom.min";
+
 import Welcome from "./pages/Welcome";
 import Products from "./pages/Products";
+import MainHeader from "./components/MainHeader";
 
 function App() {
     return (
         <div>
-        <Route path="/welcome">
-            <Welcome/>
-        </Route>
-        <Route path="/products">
-        <Products/>
-    </Route>
-    </div>
+            <MainHeader/>
+            <main>
+                <Route path="/welcome">
+                    <Welcome />
+                </Route>
+                <Route path="/products">
+                    <Products />
+                </Route>
+            </main>
+        </div>
     );
 }
 
