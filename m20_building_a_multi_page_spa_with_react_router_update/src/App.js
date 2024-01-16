37e2c1a58bead9c854a42abe431aa5f7fa13f1a6
JohnOtilla09 <johncanila.otilla@bicol-u.edu.ph>
2024-01-16 16:44:17 +0800

    React Router | 004 Defining & Using Routes

--- m20_building_a_multi_page_spa_with_react_router_update/src/App.js
+++ m20_building_a_multi_page_spa_with_react_router_update/src/App.js
@@ -1,10 +1,17 @@
-import { Router } from "react-router-dom/cjs/react-router-dom.min";
+import { Route, Router } from "react-router-dom/cjs/react-router-dom.min";
+import Welcome from "./pages/Welcome";
+import Products from "./pages/Products";
 
 function App() {
     return (
         <div>
-            <h2>Hello John C. Otilla!</h2>
-        </div>
+        <Route path="/welcome">
+            <Welcome/>
+        </Route>
+        <Route path="/products">
+        <Products/>
+    </Route>
+    </div>
     );
 }
 
